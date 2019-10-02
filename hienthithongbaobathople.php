28dbb153c65ae6fc4550ee555867484917b3698c
Thienanh-682 <Thienanh-682@gmail.com>
2019-10-02 19:50:47 +0700

    add

---
+++ hienthithongbaobathople.php
@@ -0,0 +1,34 @@
+<?php
+function calc($x, $y)
+{
+    $tong = $x + $y;
+    $hieu = $x - $y;
+    $nhan = $x * $y;
+    echo "$x + $y = $tong";
+    echo "<br>";
+    echo "$x - $y = $hieu";
+    echo "<br>";
+    echo "$x * $y = $nhan";
+    echo "<br>";
+    try {
+        condition($x, $y);
+        if ($y == 0) {
+            throw new Exception(" Xảy ra ngoại lệ ");
+        } else {
+            echo "$x / $y = " . $x / $y;
+        }
+    } catch (Exception $e) {
+        echo 'Mess: ' . $e->getMessage(), "\n";
+    }
+}
+calc(9,1);
+function condition($x, $y)
+{
+    if ($y == 0) {
+        throw new Exception(" Xảy ra ngoại lệ ");
+    } else {
+        echo "$x / $y = " . $x / $y;
+    }
+}
+calc(9,0);
+?>
